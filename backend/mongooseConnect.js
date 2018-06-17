c4f03903acb19af6eca90bfde0aac38dce948083
LiangChaokai <youchoudeyu@qq.com>
2018-06-17 22:18:45 +0800

    修复mongoose的问题

--- backend/mongooseConnect.js
+++ backend/mongooseConnect.js
@@ -1,11 +1,11 @@
 /**
  * Connect to MongoDB
  */
-var mongoose = require('mongoose'),
+const mongoose = require('mongoose'),
     DB_CONN = 'mongodb://localhost:27017/',
     DB_Name = 'nodeSpider';
 
-var DB_DatabasePath = DB_CONN + DB_Name;
+const DB_DatabasePath = DB_CONN + DB_Name;
 mongoose.Promise = global.Promise;//为了解决过期的问题
 
 console.log(DB_DatabasePath);
@@ -13,13 +13,14 @@ console.log(DB_DatabasePath);
 mongoose.set('debug', false);
 
 /*mongoose会缓存命令，只要connect成功，处于其前其后的命令都会被执行，connect命令也就无所谓放哪里*/
-var db = mongoose.connect(DB_DatabasePath, {useMongoClient: true});
+mongoose.connect(DB_DatabasePath);
+const db = mongoose.connection;
 
-// db.connection.on("error", function (error) {
-//     console.log("Fail to make connection to MongoDB：" + error);
-// });
-//
-// db.connection.on("open", function () {
-//     console.log("Success to connect the MongoDB.");
-// });
+db.on("error", function (error) {
+    console.log("Fail to make connection to MongoDB：" + error);
+});
+
+db.once("open", function () {
+    console.log("Success to connect the MongoDB.");
+});
 
