84eeafa5195aa3b3b5d7a206b407520c102a5b24
LiangChaokai <youchoudeyu@qq.com>
2019-02-27 20:44:15 +0800

    去除element

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,40 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import {
-  Button,
-  Select,
-  Row,
-  Col,
-  Menu,
-  Submenu,
-  MenuItem,
-  MenuItemGroup,
-  Loading,
-  Message,
-  MessageBox,
-} from 'element-ui';
-
-Vue.component(Button.name, Button);
-Vue.component(Select.name, Select);
-// Vue.component(Row.name, Row);
-// Vue.component(Col.name, Col);
-Vue.use(Row);
-Vue.use(Col);
-Vue.use(Menu);
-Vue.use(Submenu);
-Vue.use(MenuItem);
-Vue.use(MenuItemGroup);
 
 // Vue.use(Loading.directive);
 
-Vue.prototype.$loading = Loading.service;
-Vue.prototype.$msgbox = MessageBox;
-Vue.prototype.$alert = MessageBox.alert;
-Vue.prototype.$confirm = MessageBox.confirm;
-Vue.prototype.$prompt = MessageBox.prompt;
-Vue.prototype.$notify = Notification;
-Vue.prototype.$message = Message;
 /* 或写为
  * Vue.use(Button)
  * Vue.use(Select)
