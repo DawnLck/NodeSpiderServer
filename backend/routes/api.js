90633f8dd2f6fdc13b5659b14a0e21e10fa3d44f
LiangChaokai <youchoudeyu@qq.com>
2019-03-17 20:58:20 +0800

    尝试设置cors

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,6 +6,18 @@
 const express = require("express");
 const puppeteer = require("../process/puppteerProcess/Puppeteer");
 const router = express.Router();
+/**
+ * CORS设置
+ */
+//设置允许跨域访问该服务.
+router.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
 router.get("/", function(req, res) {
   console.log("sss");
